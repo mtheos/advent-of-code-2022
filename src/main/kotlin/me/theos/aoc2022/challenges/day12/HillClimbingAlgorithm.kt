08a0774233b3131370fa8df4644614cbfc8d1278
Michael Theos <michael@theos.me>
2022-12-14 22:00:17 +1100

    tidy

--- src/main/kotlin/me/theos/aoc2022/challenges/day12/HillClimbingAlgorithm.kt
+++ src/main/kotlin/me/theos/aoc2022/challenges/day12/HillClimbingAlgorithm.kt
@@ -2,6 +2,7 @@ package me.theos.aoc2022.challenges.day12
 
 import me.theos.aoc2022.challenges.Challenge
 import me.theos.aoc2022.challenges.Utils
+import java.awt.Point
 import java.util.PriorityQueue
 
 class HillClimbingAlgorithm : Challenge {
@@ -16,11 +17,11 @@ class HillClimbingAlgorithm : Challenge {
   }
 
   override fun solveHard(): String {
-    val startingPoints = mutableListOf<Pair<Int, Int>>().apply {
+    val startingPoints = mutableListOf<Point>().apply {
       mountain.terrain.chunked(mountain.terrain.dimX).withIndex().forEach { line ->
         line.value.withIndex().forEach {
           if (it.value == 0) {
-            add(Pair(line.index, it.index))
+            add(Point(it.index, line.index))
           }
         }
       }
@@ -29,12 +30,11 @@ class HillClimbingAlgorithm : Challenge {
     return "Part 2: $steps"
   }
 
-  private fun bfs(start: Pair<Int, Int>, end: Pair<Int, Int>): Int {
-    val dirs = listOf(Pair(0, 1), Pair(1, 0), Pair(0, -1), Pair(-1, 0))
-    val steps = List2D<Int>(mountain.terrain.dimY, mountain.terrain.dimX).init(Int.MAX_VALUE)
-      .also { it[start] = 0 }
-    val visited = List2D<Boolean>(mountain.terrain.dimY, mountain.terrain.dimX).init(false)
-    val q = PriorityQueue<Pair<Int, Int>>(Comparator { o1, o2 -> steps[o1] - steps[o2] })
+  private fun bfs(start: Point, end: Point): Int {
+    val dirs = listOf(Point(0, 1), Point(1, 0), Point(0, -1), Point(-1, 0))
+    val steps = List2D(mountain.terrain.dimY, mountain.terrain.dimX, Int.MAX_VALUE).also { it[start] = 0 }
+    val visited = List2D(mountain.terrain.dimY, mountain.terrain.dimX, false)
+    val q = PriorityQueue<Point> { o1, o2 -> steps[o1] - steps[o2] }
       .also { it.add(start) }
 
     while (q.size > 0) {
@@ -44,9 +44,9 @@ class HillClimbingAlgorithm : Challenge {
         return steps[cur]
       }
       dirs.forEach { dir ->
-        val next = Pair(cur.first + dir.first, cur.second + dir.second)
-        if (next.first >= 0 && next.first < mountain.terrain.dimY
-            && next.second >= 0 && next.second < mountain.terrain.dimX) {
+        val next = Point(cur.x + dir.x, cur.y + dir.y)
+        if (next.y >= 0 && next.y < mountain.terrain.dimY
+            && next.x >= 0 && next.x < mountain.terrain.dimX) {
           if (!visited[next]) {
             if (mountain.terrain[next] - mountain.terrain[cur] <= 1) {
               val nextSteps = steps[cur] + 1
@@ -63,8 +63,8 @@ class HillClimbingAlgorithm : Challenge {
   }
 
   private fun parseInput(input: List<String>): Mountain {
-    var start: Pair<Int, Int>? = null
-    var end: Pair<Int, Int>? = null
+    var start: Point? = null
+    var end: Point? = null
     val dimY = input.size
     val dimX = input.first().length
     val terrain = mutableListOf<Int>().apply {
@@ -72,11 +72,11 @@ class HillClimbingAlgorithm : Challenge {
         line.value.asIterable().withIndex().map { point ->
           add(when (point.value) {
             'S' -> {
-              start = Pair(line.index, point.index)
+              start = Point(point.index, line.index)
               0
             }
             'E' -> {
-              end = Pair(line.index, point.index)
+              end = Point(point.index, line.index)
               'z' - 'a'
             }
             else -> point.value - 'a'
@@ -87,15 +87,15 @@ class HillClimbingAlgorithm : Challenge {
     return Mountain(List2D.fromList(terrain, dimY, dimX), start!!, end!!)
   }
 
-  private class Mountain(val terrain: List2D<Int>, val start: Pair<Int, Int>, val end: Pair<Int, Int>) {
-    operator fun get(p: Pair<Int, Int>): Int = terrain[p]
+  private class Mountain(val terrain: List2D<Int>, val start: Point, val end: Point) {
+    operator fun get(p: Point): Int = terrain[p]
     operator fun get(y: Int, x: Int): Int = terrain[y, x]
   }
 
-  private class List2D<T>(val dimY: Int, val dimX: Int) : ArrayList<T?>() {
+  private class List2D<T>(val dimY: Int, val dimX: Int, fillWith: T) : ArrayList<T>() {
     companion object {
       fun <T> fromList(input: List<T>, dimY: Int, dimX: Int): List2D<T> {
-        return List2D<T>(dimY, dimX).apply {
+        return List2D(dimY, dimX, input[0]).apply {
           input.chunked(dimX).withIndex().forEach { row ->
             row.value.withIndex().forEach {
               this[row.index, it.index] = it.value
@@ -105,32 +105,23 @@ class HillClimbingAlgorithm : Challenge {
       }
     }
 
-    init { // more useful as a 2d array, so null everything to set the bounds
+    init {
       repeat(dimY) {
         repeat(dimX) {
-          add(null)
+          add(fillWith)
         }
       }
     }
 
-    fun init(v: T): List2D<T> { // ...even better without NPEs
-      repeat(dimY) {y ->
-        repeat(dimX) {x ->
-          this[y, x] = v
-        }
-      }
-      return this
-    }
-
-    operator fun get(p: Pair<Int, Int>): T {
-      return this[p.first, p.second]
+    operator fun get(p: Point): T {
+      return this[p.y, p.x]
     }
     operator fun get(y: Int, x: Int): T {
       checkBounds(y, x)
-      return this[y * dimX + x]!!
+      return this[y * dimX + x]
     }
-    operator fun set(p: Pair<Int, Int>, v: T) {
-      this[p.first, p.second] = v
+    operator fun set(p: Point, v: T) {
+      this[p.y, p.x] = v
     }
     operator fun set(y: Int, x: Int, v: T) {
       checkBounds(y, x)
@@ -138,10 +129,10 @@ class HillClimbingAlgorithm : Challenge {
     }
 
     private fun checkBounds(y: Int, x: Int) {
-      if (y * dimX > size) {
+      if (y * dimX >= size) {
         throw ArrayIndexOutOfBoundsException("$y is out of bounds for size $size")
       }
-      if (y * dimX + x > size) {
+      if (y * dimX + x >= size) {
         throw ArrayIndexOutOfBoundsException("($y, $x) is out of bounds for size $size. Attempted to access idx ${y * dimX + x}. Dims ($dimY, $dimX).")
       }
     }
